d70a3c3b1b00f188c317ca814b9093bdb397626c
Kevin Pai <KPai555@Hotmail.com>
2019-05-07 14:04:11 -0700

    Added opacity hover function

--- Calculator.js
+++ Calculator.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 	$(".select").hover(function() {
-		$(this).css("filter", "opacity(80%)");
+		$(this).css("filter", "opacity(70%)");
 		$(this).css("transition","filter 0.4s");
 	}, function() {
 		$(this).css("filter", "none");
