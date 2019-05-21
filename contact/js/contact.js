363c3330db25e5f0376616c0b3c591e36a7d7f6d
Kevin Pai <KPai555@Hotmail.com>
2019-05-20 17:07:10 -0700

    Clearing files for replacement

--- contact/js/contact.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-    $(".iformsub").hover(function() {
-    $(this).css("background-color", "#5cbdc5");
-  }, function() {
-    $(this).css("background-color", "#0CAA55");
-  })
-});
-
-
